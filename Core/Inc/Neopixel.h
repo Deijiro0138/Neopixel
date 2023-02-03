51d2e9adf73faf4e2bc3b80f01a0a82856b15f89
denichiro <87576918+Deijiro0138@users.noreply.github.com>
2023-02-03 21:14:21 +0900

    Initial commit

---
+++ Core/Inc/Neopixel.h
@@ -0,0 +1,11 @@
+#ifndef NEOPIXEL_H
+#define NEOPIXEL_H
+
+#include <stdio.h>
+#include <stdbool.h>
+#include "main.h"
+
+void  NeoPixInit (TIM_HandleTypeDef* _htim, uint32_t channel);
+void  NeoPixStart (uint8_t* data, int len, bool wait = true);
+
+#endif /* NEOPIXEL_H */
